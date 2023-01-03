eaca1edfb62bbfbce2c74db2ec323a534a010e76
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-01-03 20:44:30 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 18028 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i>=0; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int sum = A + B;
+			
+			if(A == 0 && B == 0 ) {
+				break;
+			}else {
+				System.out.println(sum);
+			}
+		}
+	}
+}
