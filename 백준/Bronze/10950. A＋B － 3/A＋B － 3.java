c4eafa9fec82d4d41c59f23e2e19b638fe55d60a
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-01-03 20:44:10 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17908 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = 0;
+		int B = 0;
+		int T = sc.nextInt();
+		
+		for(int i = 0; i < T; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println(A + B);
+			
+		}
+	}
+
+}
