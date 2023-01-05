b7a85d5be49025ffd460bde417a86508828d198f
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-01-05 12:58:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char[] alphabet = sc.nextLine().toCharArray();
+		
+		int a =alphabet[0];
+		
+		System.out.println(a);
+		
+	}
+
+}
