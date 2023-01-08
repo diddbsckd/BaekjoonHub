9358b73119b38ab72bce7c0c0fded4c0ff6ae905
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-01-08 17:55:05 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(int i=0; i<babbling.length; i++) {
-        	babbling[i] = babbling[i].replaceAll("aya", " ");
+        	babbling[i] = babbling[i].replace("aya", " ");
         	babbling[i] = babbling[i].replace("ye", " ");
         	babbling[i] = babbling[i].replace("woo", " ");
         	babbling[i] = babbling[i].replace("ma", " ");
