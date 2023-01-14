d84a76a66a0120b6066406cca6703c3e44124bac
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-01-14 16:44:26 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
@@ -2,12 +2,7 @@ class Solution {
     public int solution(int M, int N) {
         int answer = 0;
         
-        if(M>0 && N>0) {
-            answer += M-1;
-            for(int i=M; i>0; i--) {
-                answer += N-1;
-            }
-        }
+        answer = M*N-1;
         
         return answer;
     }
