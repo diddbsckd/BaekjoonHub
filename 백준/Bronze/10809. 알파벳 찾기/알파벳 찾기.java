1501b4f9c489c9d0eae5657dba27358ac3d83797
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-01-29 02:04:49 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String S = br.readLine();
+		String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		int[] A = new int[26];
+		
+		for(int i=0; i<alphabet.length; i++) {
+			A[i] = S.indexOf(alphabet[i]);
+			bw.write(A[i]+" ");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
