aaa6df6e00d2120bebb40577f8a17cde94bf69cf
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-02-02 16:20:44 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -3,15 +3,11 @@ class Solution {
         int answer = 0;
         int rem = 0;
         
-        while(chicken > 1) {
+        while(chicken >= 10) {
         	rem = chicken % 10;
         	chicken /= 10;
         	answer += chicken;
         	chicken += rem; 
-        	if(chicken < 10) {
-        		rem += chicken;
-        		chicken /= 10;
-        	}
         }
         
         return answer;
