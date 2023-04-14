a0f7aaec10db2ab1c0f5a9741a3a2ba353339a97
양윤창 <117149862+diddbsckd@users.noreply.github.com>
2023-04-14 22:14:20 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        int a = n/10;
+        
+        if(k >= a) {
+        	k -= a;
+        }else if(k < a) {
+        	k = 0;
+        }
+        
+        answer = (n * 12000) + (k * 2000);
+        return answer;
+    }
+}
